cccd7744a56a941b45d320893dad3c8f7ca369bd
saimushi <saimushi@10.1.109.222>
2016-04-04 17:09:00 +0900

    GRANTS修正

--- FrameworkManager/core/createdb.sql
+++ FrameworkManager/core/createdb.sql
@@ -2,4 +2,4 @@
 -- Create DB --
 CREATE DATABASE IF NOT EXISTS `fwm` CHARACTER SET utf8 COLLATE utf8_general_ci;
 -- GRANT --
-GRANT ALL ON `fwm`.* TO 'fwmuser'@'%' IDENTIFIED BY 'fwmpass'; 
+GRANT ALL ON `fwm`.* TO 'fwmuser'@"localhost" IDENTIFIED BY 'fwmpass'; 
